864833ec858918007b9e787a969f8135f401e736
Chris <christopher.schmidt89@gmx.at>
2021-11-19 22:24:38 +0100

    Es wurde alles implementiert, allerdings musste ich die methoden auf public setzen, das ich nicht weißt wie man private methoden testen soll

--- src/main/java/at/campus02/schmidt/Animal.java
+++ src/main/java/at/campus02/schmidt/Animal.java
@@ -2,9 +2,70 @@ package at.campus02.schmidt;
 
 public class Animal {
 
+    private String name;
+    private String species;
+    private String animalLoud;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getAnimalLoud() {
+        return animalLoud;
+    }
+
+    public void setAnimalLoud(String animalLoud) {
+        this.animalLoud = animalLoud;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
 
+    public Animal(String name, String species, String animalLoud, int age) {
+        this.name = name;
+        this.species = species;
+        this.animalLoud = animalLoud;
+        this.age = age;
+    }
 
+        public String giveAnimalLoud(){
+        return animalLoud;
+
+    }
+        public String getNameAndAge(){
+        String nameAndAge= "Das Tier heißt "+name+ " und ist "+age+" alt";
+        return nameAndAge;
+
+    }
+
+        public String ageCategory(){
+        String ausgabe ="";
+        if(age>4){ ausgabe= "old animal";}
+        if(age>2){ ausgabe= "youg animal";}
+        if(age<2 && age>0){ ausgabe= "baby animal";}
+
+        return ausgabe;
+
+    }
 
 
 
--- src/test/java/at/campus02/schmidt/AnimalTest.java
+++ src/test/java/at/campus02/schmidt/AnimalTest.java
@@ -1,18 +1,43 @@
 package at.campus02.schmidt;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalTest {
-    int a;
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {a=1;
+
+    private Animal säugetier;
+
+
+    @BeforeEach
+    void setUp() {
+        säugetier = new Animal("Löwe", "Säugetier", "roar", 1);
+
     }
+
     @Test
-    void myFirstTest(){
-        assertTrue(a>0);
+    void giveAnimalLoud () {
+
+    String tierlaut = säugetier.giveAnimalLoud();
+    assertEquals(tierlaut, "roar");
+
+
+
+    }
+
+    @Test
+    void getNameAndAge () {
+        String nameUndAlter = säugetier.getNameAndAge();
+        assertEquals(nameUndAlter, "Das Tier heißt Löwe und ist 1 alt");
+
+    }
+
+    @Test
+    void ageCategory() {
+        String altersKategorie = säugetier.ageCategory();
+        assertEquals(altersKategorie, "baby animal");
 
     }
 }
